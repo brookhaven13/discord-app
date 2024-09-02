235f42e342402df02747c18ff0b2d5aaeba92bb3
Harley Lin <harley.lin@coretronic.com>
2024-09-02 11:39:15 +0800

    feat: 🎸 finish create server button tooltip

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -2,16 +2,21 @@
 
 import { Plus } from "lucide-react";
 
+import { ActionTooltip } from "@/components/action-tooltip";
+
 export const NavigationAction = () => {
   return (
     <div>
-      <button
-        className="group"
-      >
-        <div className="flex items-center justify-center mx-3 h-12 w-12 rounded-3xl group-hover:rounded-2xl transition-all overflow-hidden bg-background dark:bg-neutral-700 group-hover:bg-emerald-500">
-          <Plus className="group-hover:text-white transition text-emerald-500" size={25} />
-        </div>
-      </button>
+      <ActionTooltip side="right" align="center" label="Add a server">
+        <button className="group flex items-center">
+          <div className="flex items-center justify-center mx-3 h-12 w-12 rounded-3xl group-hover:rounded-2xl transition-all overflow-hidden bg-background dark:bg-neutral-700 group-hover:bg-emerald-500">
+            <Plus
+              className="group-hover:text-white transition text-emerald-500"
+              size={25}
+            />
+          </div>
+        </button>
+      </ActionTooltip>
     </div>
   );
 };
