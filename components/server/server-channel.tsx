e5d33f4f2239e50bfbe3fc07f8808cfdc50b96be
Harley <yu.h.lin1991@gmail.com>
2024-09-22 01:07:11 +0800

    fix: 🐛 fix typo

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -46,7 +46,7 @@ const ServerChannel = ({ channel, server, role }: ServerChannelProps) => {
           <ActionTooltip label="Edit">
             <Edit className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
           </ActionTooltip>
-          <ActionTooltip label="Edit">
+          <ActionTooltip label="Delete">
             <Trash className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
           </ActionTooltip>
         </div>
